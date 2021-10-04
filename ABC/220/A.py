995486aeba7fd2dfe925899174fde56a67aa32cf
ak1ya <sasanoha.payapaya@gmail.com>
2021-10-04 14:40:39 +0900

    ABC220A

--- ABC/220/A.py
+++ ABC/220/A.py
@@ -1,6 +1,6 @@
-A, B, C = map(int, input().split())
-for i in range(A, B+1):
-    if i%C == 0:
+a, b, c = map(int, input().split())
+for i in range(a, b+1):
+    if i%c == 0:
         print(i)
         exit()
 print(-1)
